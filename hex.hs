94882b064b4216226fb019f09abf45d84fe7b63d
John MacFarlane <jgm@berkeley.edu>
2010-08-18 09:44:14 -0700

    Improved error handling in hex.

--- hex.hs
+++ hex.hs
@@ -1,10 +1,16 @@
 import Language.Haskell.Interpreter
 import System.Environment
 import qualified Data.ByteString.Lazy as L
+import System.Exit
+import System.IO
 
 main :: IO ()
 main = do
-  (file:fmt:_) <- getArgs
+  args <- getArgs
+  (file,fmt) <- case args of
+                     [x,y]   -> return (x,y)
+                     _       -> do hPutStrLn stderr $ "Usage:  hex FILE FORMAT"
+                                   exitWith $ ExitFailure 1
   res <- runInterpreter $ do
     loadModules [file]
     setTopLevelModules ["Main"]
@@ -17,3 +23,4 @@ main = do
         Left (GhcException s)    -> error s
         Left err                 -> error $ show err
         Right f                  -> L.putStr =<< f file fmt
+  exitWith ExitSuccess
