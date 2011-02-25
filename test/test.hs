452072d815d25bed7c57ba0bf82b359551ce95b4
John MacFarlane <jgm@berkeley.edu>
2011-02-25 13:00:55 -0800

    Updated test.hs

--- test/test.hs
+++ test/test.hs
@@ -2,7 +2,8 @@
 import Text.HeX
 import Text.HeX.Standard as Standard
 import Docbook as Docbook
-import Control.Monad (guard)
+import Control.Monad (guard, liftM)
+import qualified Data.Map as M
 import Text.HeX.Standard.Xml (inTags)
 import Text.HeX.Standard.TeX (ctl,grp)
 
@@ -11,6 +12,8 @@ main = defaultMain $ do
   forFormat "docbook" Docbook.defaults
   register "silly" silly
   register "lettrine" lettrine
+  addParser Math unknown
+  -- addParser Normal unknown
   parseDoc
 
 silly :: OptionList -> Doc
@@ -20,6 +23,15 @@ silly (OptionList opts) =
 lettrine :: Format -> Doc -> Doc -> Doc
 lettrine "html" x y = inTags "span" [("class","lettrine")] x +++ y
 lettrine "latex" x y = ctl "lettrine" +++ grp [x] +++ grp [y]
+
+unknown :: HeX Doc
+unknown = try $ do
+  char '\\'
+  cmd <- many1 letter <|> count 1 anyChar
+  commands <- liftM hexCommands getState
+  case M.lookup cmd commands of
+       Nothing -> return mempty
+       Just _  -> fail "known command"
 
 -- CSS:
 -- body { line-height: 1.3; }
