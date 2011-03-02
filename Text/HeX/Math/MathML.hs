66e8ee836385b2d913d614d9f5ba8d63b8e2f3ef
John MacFarlane <jgm@berkeley.edu>
2011-03-02 14:23:46 -0800

    Copied texmath array code into comments in MathML module.

--- Text/HeX/Math/MathML.hs
+++ Text/HeX/Math/MathML.hs
@@ -546,330 +546,78 @@ superscript :: HeX Doc
 superscript = try $ char '^' >> math
 
 {-
-  -- limits <- limitsIndicator
-  -- subSup limits a <|> superOrSubscripted limits a <|> return a
 
-expr :: GenParser Char st Exp
-expr = do
-  a <- expr1
-  limits <- limitsIndicator
-  subSup limits a <|> superOrSubscripted limits a <|> return a
+endLine :: GenParser Char st Char
+endLine = try $ do
+  symbol "\\\\"
+  optional inbrackets  -- can contain e.g. [1.0in] for a line height, not yet supported
+  return '\n'
 
-limitsIndicator :: GenParser Char st (Maybe Bool)
-limitsIndicator =
-   try (symbol "\\limits" >> return (Just True))
-  <|> try (symbol "\\nolimits" >> return (Just False))
-  <|> return Nothing
+arrayLine :: GenParser Char st ArrayLine
+arrayLine = notFollowedBy (try $ char '\\' >> symbol "end" >> return '\n') >>
+  sepBy1 (many (notFollowedBy endLine >> expr)) (symbol "&")
 
-subSup :: Maybe Bool -> Exp -> GenParser Char st Exp
-subSup limits a = try $ do
-  char '_'
-  b <- expr1
-  char '^'
-  c <- expr
-  return $ case limits of
-            Just True  -> EUnderover a b c
-            Nothing | isConvertible a -> EDownup a b c
-            _          -> ESubsup a b c
+array :: GenParser Char st Exp
+array = stdarray <|> eqnarray <|> align <|> cases <|> matrix
 
-superOrSubscripted :: Maybe Bool -> Exp -> GenParser Char st Exp
-superOrSubscripted limits a = try $ do
-  c <- oneOf "^_"
-  b <- expr
-  case c of
-       '^' -> return $ case limits of
-                        Just True  -> EOver a b
-                        Nothing | isConvertible a -> EUp a b
-                        _          -> ESuper a b
-       '_' -> return $ case limits of
-                        Just True  -> EUnder a b
-                        Nothing | isConvertible a -> EDown a b
-                        _          -> ESub a b
-       _   -> pzero
+matrix :: GenParser Char st Exp
+matrix =  matrixWith "pmatrix" "(" ")"
+      <|> matrixWith "bmatrix" "[" "]"
+      <|> matrixWith "Bmatrix" "{" "}"
+      <|> matrixWith "vmatrix" "\x2223" "\x2223"
+      <|> matrixWith "Vmatrix" "\x2225" "\x2225"
 
-isConvertible :: Exp -> Bool
-isConvertible (EMathOperator x) = x `elem` convertibleOps
-  where convertibleOps = ["lim","liminf","limsup","inf","sup"]
-isConvertible (ESymbol Rel _) = True
-isConvertible (ESymbol Bin _) = True
-isConvertible (EUnder _ _)    = True
-isConvertible (EOver _ _)     = True
-isConvertible (EUnderover _ _ _) = True
-isConvertible (ESymbol Op x) = x `elem` convertibleSyms
-  where convertibleSyms = ["\x2211","\x220F","\x22C2",
-           "\x22C3","\x22C0","\x22C1","\x2A05","\x2A06",
-           "\x2210","\x2A01","\x2A02","\x2A00","\x2A04"]
-isConvertible _ = False
+matrixWith :: String -> String -> String -> GenParser Char st Exp
+matrixWith keywd opendelim closedelim =
+  inEnvironment keywd $ do
+    aligns <- option [] arrayAlignments
+    lines' <- sepEndBy1 arrayLine endLine
+    return $ EGrouped [ EStretchy (ESymbol Open opendelim)
+                      , EArray aligns lines'
+                      , EStretchy (ESymbol Close closedelim)]
 
-symbols :: M.Map String Exp
-symbols = M.fromList [
-             ("+", ESymbol Bin "+")
-           , ("-", ESymbol Bin "-")
-           , ("*", ESymbol Bin "*")
-           , (",", ESymbol Pun ",")
-           , (".", ESymbol Pun ".")
-           , (";", ESymbol Pun ";")
-           , (":", ESymbol Pun ":")
-           , ("?", ESymbol Pun "?")
-           , (">", ESymbol Rel ">")
-           , ("<", ESymbol Rel "<")
-           , ("!", ESymbol Ord "!")
-           , ("'", ESymbol Ord "\x02B9")
-           , ("''", ESymbol Ord "\x02BA")
-           , ("'''", ESymbol Ord "\x2034")
-           , ("''''", ESymbol Ord "\x2057")
-           , ("=", ESymbol Rel "=")
-           , (":=", ESymbol Rel ":=")
-           , ("\\mid", ESymbol Bin "\x2223")
-           , ("\\parallel", ESymbol Rel "\x2225")
-           , ("\\backslash", ESymbol Bin "\x2216")
-           , ("/", ESymbol Bin "/")
-           , ("\\setminus",	ESymbol Bin "\\")
-           , ("\\times", ESymbol Bin "\x00D7")
-           , ("\\alpha", ESymbol Ord "\x03B1")
-           , ("\\beta", ESymbol Ord "\x03B2")
-           , ("\\chi", ESymbol Ord "\x03C7")
-           , ("\\delta", ESymbol Ord "\x03B4")
-           , ("\\Delta", ESymbol Op "\x0394")
-           , ("\\epsilon", ESymbol Ord "\x03B5")
-           , ("\\varepsilon", ESymbol Ord "\x025B")
-           , ("\\eta", ESymbol Ord "\x03B7")
-           , ("\\gamma", ESymbol Ord "\x03B3")
-           , ("\\Gamma", ESymbol Op "\x0393") 
-           , ("\\iota", ESymbol Ord "\x03B9")
-           , ("\\kappa", ESymbol Ord "\x03BA")
-           , ("\\lambda", ESymbol Ord "\x03BB")
-           , ("\\Lambda", ESymbol Op "\x039B") 
-           , ("\\mu", ESymbol Ord "\x03BC")
-           , ("\\nu", ESymbol Ord "\x03BD")
-           , ("\\omega", ESymbol Ord "\x03C9")
-           , ("\\Omega", ESymbol Op "\x03A9")
-           , ("\\phi", ESymbol Ord "\x03C6")
-           , ("\\varphi", ESymbol Ord "\x03D5")
-           , ("\\Phi", ESymbol Op "\x03A6") 
-           , ("\\pi", ESymbol Ord "\x03C0")
-           , ("\\Pi", ESymbol Op "\x03A0") 
-           , ("\\psi", ESymbol Ord "\x03C8")
-           , ("\\Psi", ESymbol Ord "\x03A8")
-           , ("\\rho", ESymbol Ord "\x03C1")
-           , ("\\sigma", ESymbol Ord "\x03C3")
-           , ("\\Sigma", ESymbol Op "\x03A3") 
-           , ("\\tau", ESymbol Ord "\x03C4")
-           , ("\\theta", ESymbol Ord "\x03B8")
-           , ("\\vartheta", ESymbol Ord "\x03D1")
-           , ("\\Theta", ESymbol Op "\x0398") 
-           , ("\\upsilon", ESymbol Ord "\x03C5")
-           , ("\\xi", ESymbol Ord "\x03BE")
-           , ("\\Xi", ESymbol Op "\x039E") 
-           , ("\\zeta", ESymbol Ord "\x03B6")
-           , ("\\frac12", ESymbol Ord "\x00BD")
-           , ("\\frac14", ESymbol Ord "\x00BC")
-           , ("\\frac34", ESymbol Ord "\x00BE")
-           , ("\\frac13", ESymbol Ord "\x2153")
-           , ("\\frac23", ESymbol Ord "\x2154")
-           , ("\\frac15", ESymbol Ord "\x2155")
-           , ("\\frac25", ESymbol Ord "\x2156")
-           , ("\\frac35", ESymbol Ord "\x2157")
-           , ("\\frac45", ESymbol Ord "\x2158")
-           , ("\\frac16", ESymbol Ord "\x2159")
-           , ("\\frac56", ESymbol Ord "\x215A")
-           , ("\\frac18", ESymbol Ord "\x215B")
-           , ("\\frac38", ESymbol Ord "\x215C")
-           , ("\\frac58", ESymbol Ord "\x215D")
-           , ("\\frac78", ESymbol Ord "\x215E")
-           , ("\\pm", ESymbol Bin "\x00B1")
-           , ("\\mp", ESymbol Bin "\x2213")
-           , ("\\triangleleft", ESymbol Bin "\x22B2")
-           , ("\\triangleright", ESymbol Bin "\x22B3")
-           , ("\\cdot", ESymbol Bin "\x22C5")
-           , ("\\star", ESymbol Bin "\x22C6")
-           , ("\\ast", ESymbol Bin "\x002A")
-           , ("\\times", ESymbol Bin "\x00D7")
-           , ("\\div", ESymbol Bin "\x00F7")
-           , ("\\circ", ESymbol Bin "\x2218")
-           , ("\\bullet", ESymbol Bin "\x2022")
-           , ("\\oplus", ESymbol Bin "\x2295")
-           , ("\\ominus", ESymbol Bin "\x2296")
-           , ("\\otimes", ESymbol Bin "\x2297")
-           , ("\\bigcirc", ESymbol Bin "\x25CB")
-           , ("\\oslash", ESymbol Bin "\x2298")
-           , ("\\odot", ESymbol Bin "\x2299")
-           , ("\\land", ESymbol Bin "\x2227")
-           , ("\\wedge", ESymbol Bin "\x2227")
-           , ("\\lor", ESymbol Bin "\x2228")
-           , ("\\vee", ESymbol Bin "\x2228")
-           , ("\\cap", ESymbol Bin "\x2229")
-           , ("\\cup", ESymbol Bin "\x222A")
-           , ("\\sqcap", ESymbol Bin "\x2293")
-           , ("\\sqcup", ESymbol Bin "\x2294")
-           , ("\\uplus", ESymbol Bin "\x228E")
-           , ("\\amalg", ESymbol Bin "\x2210")
-           , ("\\bigtriangleup", ESymbol Bin "\x25B3")
-           , ("\\bigtriangledown", ESymbol Bin "\x25BD")
-           , ("\\dag", ESymbol Bin "\x2020")
-           , ("\\dagger", ESymbol Bin "\x2020")
-           , ("\\ddag", ESymbol Bin "\x2021")
-           , ("\\ddagger", ESymbol Bin "\x2021")
-           , ("\\lhd", ESymbol Bin "\x22B2")
-           , ("\\rhd", ESymbol Bin "\x22B3")
-           , ("\\unlhd", ESymbol Bin "\x22B4")
-           , ("\\unrhd", ESymbol Bin "\x22B5")
-           , ("\\lt", ESymbol Rel "<")
-           , ("\\gt", ESymbol Rel ">")
-           , ("\\ne", ESymbol Rel "\x2260")
-           , ("\\neq", ESymbol Rel "\x2260")
-           , ("\\le", ESymbol Rel "\x2264")
-           , ("\\leq", ESymbol Rel "\x2264")
-           , ("\\leqslant", ESymbol Rel "\x2264")
-           , ("\\ge", ESymbol Rel "\x2265")
-           , ("\\geq", ESymbol Rel "\x2265")
-           , ("\\geqslant", ESymbol Rel "\x2265")
-           , ("\\equiv", ESymbol Rel "\x2261")
-           , ("\\ll", ESymbol Rel "\x226A")
-           , ("\\gg", ESymbol Rel "\x226B")
-           , ("\\doteq", ESymbol Rel "\x2250")
-           , ("\\prec", ESymbol Rel "\x227A")
-           , ("\\succ", ESymbol Rel "\x227B")
-           , ("\\preceq", ESymbol Rel "\x227C")
-           , ("\\succeq", ESymbol Rel "\x227D")
-           , ("\\subset", ESymbol Rel "\x2282")
-           , ("\\supset", ESymbol Rel "\x2283")
-           , ("\\subseteq", ESymbol Rel "\x2286")
-           , ("\\supseteq", ESymbol Rel "\x2287")
-           , ("\\sqsubset", ESymbol Rel "\x228F")
-           , ("\\sqsupset", ESymbol Rel "\x2290")
-           , ("\\sqsubseteq", ESymbol Rel "\x2291")
-           , ("\\sqsupseteq", ESymbol Rel "\x2292")
-           , ("\\sim", ESymbol Rel "\x223C")
-           , ("\\simeq", ESymbol Rel "\x2243")
-           , ("\\approx", ESymbol Rel "\x2248")
-           , ("\\cong", ESymbol Rel "\x2245")
-           , ("\\Join", ESymbol Rel "\x22C8")
-           , ("\\bowtie", ESymbol Rel "\x22C8")
-           , ("\\in", ESymbol Rel "\x2208")
-           , ("\\ni", ESymbol Rel "\x220B")
-           , ("\\owns", ESymbol Rel "\x220B")
-           , ("\\propto", ESymbol Rel "\x221D")
-           , ("\\vdash", ESymbol Rel "\x22A2")
-           , ("\\dashv", ESymbol Rel "\x22A3")
-           , ("\\models", ESymbol Rel "\x22A8")
-           , ("\\perp", ESymbol Rel "\x22A5")
-           , ("\\smile", ESymbol Rel "\x2323")
-           , ("\\frown", ESymbol Rel "\x2322")
-           , ("\\asymp", ESymbol Rel "\x224D")
-           , ("\\notin", ESymbol Rel "\x2209")
-           , ("\\gets", ESymbol Rel "\x2190")
-           , ("\\leftarrow", ESymbol Rel "\x2190")
-           , ("\\to", ESymbol Rel "\x2192")
-           , ("\\rightarrow", ESymbol Rel "\x2192")
-           , ("\\leftrightarrow", ESymbol Rel "\x2194")
-           , ("\\uparrow", ESymbol Rel "\x2191")
-           , ("\\downarrow", ESymbol Rel "\x2193")
-           , ("\\updownarrow", ESymbol Rel "\x2195")
-           , ("\\Leftarrow", ESymbol Rel "\x21D0")
-           , ("\\Rightarrow", ESymbol Rel "\x21D2")
-           , ("\\Leftrightarrow", ESymbol Rel "\x21D4")
-           , ("\\iff", ESymbol Rel "\x21D4")
-           , ("\\Uparrow", ESymbol Rel "\x21D1")
-           , ("\\Downarrow", ESymbol Rel "\x21D3")
-           , ("\\Updownarrow", ESymbol Rel "\x21D5")
-           , ("\\mapsto", ESymbol Rel "\x21A6")
-           , ("\\longleftarrow", ESymbol Rel "\x2190")
-           , ("\\longrightarrow", ESymbol Rel "\x2192")
-           , ("\\longleftrightarrow", ESymbol Rel "\x2194")
-           , ("\\Longleftarrow", ESymbol Rel "\x21D0")
-           , ("\\Longrightarrow", ESymbol Rel "\x21D2")
-           , ("\\Longleftrightarrow", ESymbol Rel "\x21D4")
-           , ("\\longmapsto", ESymbol Rel "\x21A6")
-           , ("\\sum", ESymbol Op "\x2211")
-           , ("\\prod", ESymbol Op "\x220F")
-           , ("\\bigcap", ESymbol Op "\x22C2")
-           , ("\\bigcup", ESymbol Op "\x22C3")
-           , ("\\bigwedge", ESymbol Op "\x22C0")
-           , ("\\bigvee", ESymbol Op "\x22C1")
-           , ("\\bigsqcap", ESymbol Op "\x2A05")
-           , ("\\bigsqcup", ESymbol Op "\x2A06")
-           , ("\\coprod", ESymbol Op "\x2210")
-           , ("\\bigoplus", ESymbol Op "\x2A01")
-           , ("\\bigotimes", ESymbol Op "\x2A02")
-           , ("\\bigodot", ESymbol Op "\x2A00")
-           , ("\\biguplus", ESymbol Op "\x2A04")
-           , ("\\int", ESymbol Op "\x222B")
-           , ("\\iint", ESymbol Op "\x222C")
-           , ("\\iiint", ESymbol Op "\x222D")
-           , ("\\oint", ESymbol Op "\x222E")
-           , ("\\prime", ESymbol Ord "\x2032")
-           , ("\\dots", ESymbol Ord "\x2026")
-           , ("\\ldots", ESymbol Ord "\x2026")
-           , ("\\cdots", ESymbol Ord "\x22EF")
-           , ("\\vdots", ESymbol Ord "\x22EE")
-           , ("\\ddots", ESymbol Ord "\x22F1")
-           , ("\\forall", ESymbol Op "\x2200")
-           , ("\\exists", ESymbol Op "\x2203")
-           , ("\\Re", ESymbol Ord "\x211C")
-           , ("\\Im", ESymbol Ord "\x2111")
-           , ("\\aleph", ESymbol Ord "\x2135")
-           , ("\\hbar", ESymbol Ord "\x210F")
-           , ("\\ell", ESymbol Ord "\x2113")
-           , ("\\wp", ESymbol Ord "\x2118")
-           , ("\\emptyset", ESymbol Ord "\x2205")
-           , ("\\infty", ESymbol Ord "\x221E")
-           , ("\\partial", ESymbol Ord "\x2202")
-           , ("\\nabla", ESymbol Ord "\x2207")
-           , ("\\triangle", ESymbol Ord "\x25B3")
-           , ("\\therefore", ESymbol Pun "\x2234")
-           , ("\\angle", ESymbol Ord "\x2220")
-           , ("\\diamond", ESymbol Op "\x22C4")
-           , ("\\Diamond", ESymbol Op "\x25C7")
-           , ("\\lozenge", ESymbol Op "\x25CA")
-           , ("\\neg", ESymbol Op "\x00AC")
-           , ("\\lnot", ESymbol Ord "\x00AC")
-           , ("\\bot", ESymbol Ord "\x22A5")
-           , ("\\top", ESymbol Ord "\x22A4")
-           , ("\\square", ESymbol Ord "\x25AB")
-           , ("\\Box", ESymbol Op "\x25A1")
-           , ("\\wr", ESymbol Ord "\x2240")
-           , ("\\!", ESpace "-0.167em")
-           , ("\\,", ESpace "0.167em")
-           , ("\\>", ESpace "0.222em")
-           , ("\\:", ESpace "0.222em")
-           , ("\\;", ESpace "0.278em")
-           , ("~", ESpace "0.333em")
-           , ("\\quad", ESpace "1em")
-           , ("\\qquad", ESpace "2em")
-           , ("\\arccos", EMathOperator "arccos")
-           , ("\\arcsin", EMathOperator "arcsin")
-           , ("\\arctan", EMathOperator "arctan")
-           , ("\\arg", EMathOperator "arg")
-           , ("\\cos", EMathOperator "cos")
-           , ("\\cosh", EMathOperator "cosh")
-           , ("\\cot", EMathOperator "cot")
-           , ("\\coth", EMathOperator "coth")
-           , ("\\csc", EMathOperator "csc")
-           , ("\\deg", EMathOperator "deg")
-           , ("\\det", EMathOperator "det")
-           , ("\\dim", EMathOperator "dim")
-           , ("\\exp", EMathOperator "exp")
-           , ("\\gcd", EMathOperator "gcd")
-           , ("\\hom", EMathOperator "hom")
-           , ("\\inf", EMathOperator "inf")
-           , ("\\ker", EMathOperator "ker")
-           , ("\\lg", EMathOperator "lg")
-           , ("\\lim", EMathOperator "lim")
-           , ("\\liminf", EMathOperator "liminf")
-           , ("\\limsup", EMathOperator "limsup")
-           , ("\\ln", EMathOperator "ln")
-           , ("\\log", EMathOperator "log")
-           , ("\\max", EMathOperator "max")
-           , ("\\min", EMathOperator "min")
-           , ("\\Pr", EMathOperator "Pr")
-           , ("\\sec", EMathOperator "sec")
-           , ("\\sin", EMathOperator "sin")
-           , ("\\sinh", EMathOperator "sinh")
-           , ("\\sup", EMathOperator "sup")
-           , ("\\tan", EMathOperator "tan")
-           , ("\\tanh", EMathOperator "tanh")
-           ] 
+stdarray :: GenParser Char st Exp
+stdarray = inEnvironment "array" $ do
+  aligns <- option [] arrayAlignments
+  liftM (EArray aligns) $ sepEndBy1 arrayLine endLine
+
+eqnarray :: GenParser Char st Exp
+eqnarray = inEnvironment "eqnarray" $
+  liftM (EArray [AlignRight, AlignCenter, AlignLeft]) $
+    sepEndBy1 arrayLine endLine
+
+align :: GenParser Char st Exp
+align = inEnvironment "align" $
+  liftM (EArray [AlignRight, AlignLeft]) $
+    sepEndBy1 arrayLine endLine
+
+cases :: GenParser Char st Exp
+cases = inEnvironment "cases" $ do
+  rs <- sepEndBy1 arrayLine endLine
+  return $ EGrouped [EStretchy (ESymbol Open "{"), EArray [] rs]
+
+arrayAlignments :: GenParser Char st [Alignment]
+arrayAlignments = try $ do
+  as <- braces (many letter)
+  let letterToAlignment 'l' = AlignLeft
+      letterToAlignment 'c' = AlignCenter
+      letterToAlignment 'r' = AlignRight
+      letterToAlignment _   = AlignDefault
+  return $ map letterToAlignment as
+
+inEnvironment :: String
+              -> GenParser Char st Exp
+              -> GenParser Char st Exp
+inEnvironment envType p = do
+  try $ do char '\\'
+           symbol "begin"
+           braces $ symbol envType >> optional (symbol "*")
+  result <- p
+  char '\\'
+  symbol "end"
+  braces $ symbol envType >> optional (symbol "*")
+  return result 
+
 
 
 -}
